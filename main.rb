23b3f510e449ed9384c4f5d854c6a2ae6753d061
Tony Contreras <tony.contreras@gmail.com>
2013-02-06 17:24:43 -0500

    works. need styling

---
+++ main.rb
@@ -0,0 +1,33 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'active_support/all'
+require 'httparty'
+require 'json'
+
+get '/' do
+  erb :home
+end
+
+post '/search' do
+  @search = params[:search].gsub(" ","+").gsub("'","")
+  @a = HTTParty.get("http://www.omdbapi.com/?t=#{@search}")
+  @b = JSON(@a)
+  @title = @b["Title"]
+  @rated = @b["Rated"]
+  @poster = @b["Poster"]
+
+  erb :home
+
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/faq' do
+  erb :faq
+end
+
+
+
